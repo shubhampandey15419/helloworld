0916d1418786a1d4ba5dab6c800b9198581b47c2
shubhampandey15419 <shubhampandey15419@gmail.com>
2020-02-05 00:06:25 +0530

    Add files via upload

---
+++ Geeks.java
@@ -0,0 +1,11 @@
+class Geeks { 
+  
+    public static void main(String[] args) 
+    { 
+        // When very large integer is taken 
+        int a = 92374612162;  
+        int b = 181; 
+        // It will return the sum of two arguments. 
+        System.out.println("The sum is =" + Integer.sum(a, b)); 
+    } 
+} 
